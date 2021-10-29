79a06ac590a5a8d1d7d1e547110a147c6d4af6b5
vadim <vadim444@hotmail.com>
2021-10-29 21:47:07 +0100

    working on loadingManager

--- Game/Game.js
+++ Game/Game.js
@@ -1,9 +1,13 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useMemo } from 'react'
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { Canvas, useFrame } from '@react-three/fiber'
 import { Suspense, useEffect } from 'react'
+import * as THREE from 'three'
 import { useThree } from '@react-three/fiber'
 import { OrbitControls, ContactShadows, Environment } from '@react-three/drei'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import Model from "./Components/Model";
+
 
 const Box = props => {
   // This reference will give us direct access to the THREE.Mesh object
@@ -28,13 +32,44 @@ const Box = props => {
   )
 }
 
-function RecalcShadows() {
+const RecalcShadows = () => {
   const gl = useThree((state) => state.gl)
   useEffect(() => void (gl.shadowMap.needsUpdate = true), [])
   return null
 }
 
+// const loadingManager = new THREE.LoadingManager(
+//   // Loaded
+//   () =>
+//   {
+//       // Wait a little
+//       (() =>
+//       {
+//           // Animate overlay
+
+
+//           // Update loadingBarElement
+
+//       }, 500)
+
+//       window.setTimeout(() =>
+//       {
+//           sceneReady = true
+//       }, 2000)
+//   },
+
+//   // Progress
+//   (itemUrl, itemsLoaded, itemsTotal) =>
+//   {
+//       // Calculate the progress and update the loadingBarElement
+//       const progressRatio = itemsLoaded / itemsTotal
+//       loadingBarElement.style.transform = `scaleX(${progressRatio})`
+//   }
+// )
+
 const Game = props => {
+    const [gltf, set] = useState()
+
     return (
       <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0.5, 1], fov: 50, near: 0.001 }} onCreated={(state) => (state.gl.shadowMap.autoUpdate = false)}>
         <ambientLight intensity={4} />
@@ -43,13 +78,23 @@ const Game = props => {
         <Suspense fallback={null}>
           <Box position={[-1.2, 0, 0]} />
           <Box position={[1.2, 0, 0]} />
-          {/* <Model frames={1} limit={50} position={[0, -0.0005, 0]} castShadow receiveShadow /> */}
           <ContactShadows frames={1} rotation-x={[Math.PI / 2]} position={[0, -0.4, 0]} far={1} width={1.5} height={1.5} blur={3} />
           <Environment preset="night" />
           <RecalcShadows />
         </Suspense>
+        <Model url='/models/DamagedHelmet.gltf' 
+          // loadingManager={loadingManager}
+        />
         <OrbitControls zoomSpeed={0.1} autoRotate autoRotateSpeed={0.0} rotateSpeed={2} dampingFactor={0.5} minPolarAngle={-Math.PI / 2} maxPolarAngle={Math.PI / 1.7} makeDefault />
       </Canvas>
     )
   }
+
+
+
+// const Model = ({modelPath}) => {
+//   const gltf = useLoader(GLTFLoader, modelPath, draco());
+//   return gltf ? <primitive object={gltf.scene} /> : null;
+// }
+
 export default Game
