a9af99d1ca697e5f88c6f95e9643ba3f1e39dfce
vadimstasiev <vadim444@hotmail.com>
2021-11-21 14:19:44 +0000

    math for placing asteroid

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,23 +1,25 @@
 import * as THREE from 'three'
 
 const playClicked = (scene) => {
-  for (let i = 0; i < 1000; i++) {
-        const asteroidGeometry = new THREE.SphereBufferGeometry(1, 16, 16)
-        const asteroidMaterial = new THREE.MeshStandardMaterial({ color: '#89c854' })
+    const asteroidGeometry = new THREE.SphereBufferGeometry(1, 16, 16)
+    const asteroidMaterial = new THREE.MeshStandardMaterial({ color: '#89c854' })
 
-        const asteroid1 = new THREE.Mesh(asteroidGeometry, asteroidMaterial)
-        asteroid1.castShadow = true
-        asteroid1.scale.set(1, 1, 1)
+    const asteroid1 = new THREE.Mesh(asteroidGeometry, asteroidMaterial)
+    asteroid1.castShadow = true
+    asteroid1.scale.set(1, 1, 1)
 
-        const max = 20
-        const min = 6
 
-        const x = (Math.random() * (max - min) + min) * ((Math.random()-0.5)>0?1:-1)
-        const y = 0
-        const z = (Math.random() * (max - min) + min) * ((Math.random()-0.5)>0?1:-1)
-        asteroid1.position.set(x, y, z)
-        scene.add(asteroid1)
-    }
+    // Find coordinates for a random point within the radius of 10 and outside the radius of 6
+
+    const max = 10
+    const min = 6
+    const random = Math.random() * (max) 
+    const x = Math.sin(random) *max - ((Math.random()-0.5)*min)
+    const y = 0
+    const z = Math.cos(random) *max - ((Math.random()-0.5)*min)
+    asteroid1.position.set(x, y, z)
+    scene.add(asteroid1)
+
 }
 
 export default playClicked
