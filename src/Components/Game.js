8c2f3561acbce5fd1094bc64e58e123a6e463874
vadimstasiev <vadim444@hotmail.com>
2021-11-24 16:05:04 +0000

    asteroids are placed in the right position

--- src/Components/Game.js
+++ src/Components/Game.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three'
-import { Vector3 } from 'three'
+import { Vector2, Vector3 } from 'three'
 
 const playClicked = (scene, camera) => {
     const asteroidGeometry = new THREE.SphereBufferGeometry(1, 16, 16)
@@ -9,7 +9,22 @@ const playClicked = (scene, camera) => {
     let minRange = 0
     let maxRange = 0
     const cameraDirection = camera.getWorldDirection()
+    const xzCameraDirection = new Vector3(cameraDirection.x, 1, cameraDirection.z)
 
+
+    // calculate horizontal fov
+    const hFOV = Math.abs(2 * Math.atan( Math.tan( camera.fov / 2 ) * camera.aspect )) 
+    const spawnAngle = 0.3
+    const spawnOffset =  (Math.PI/2)
+
+    const max = 10
+    const min = 6
+    const amplitudeY = 4
+    let x,y,z, theta, distanceToCamera
+    const vec3 = new Vector3()
+    const cameraAngle = Math.atan2(cameraDirection.x, cameraDirection.z)
+    var frustum = new THREE.Frustum()
+    frustum.setFromProjectionMatrix(new THREE.Matrix4().multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse))
 
     // for (let i = 0; i < 1000; i++) {
         const asteroid1 = new THREE.Mesh(asteroidGeometry, asteroidMaterial)
@@ -19,26 +34,27 @@ const playClicked = (scene, camera) => {
 
         // Find coordinates for a random point within the radius of 10 and outside the radius of 6
 
-        const max = 10
-        const min = 6
-        const amplitudeY = 4
-        let x,y,z, theta
+       
         // const cameraDirection = camera.getWorldDirection()
-        const cameraAngle = Math.atan2(cameraDirection.x, cameraDirection.z)
         
-        
+        let count = 0
+        let countMax = 100
 
-        // do {
+        do {
             const random = Math.random() *max 
             x = Math.sin(random) *max - ((Math.random()-0.5)*min)
             y = (Math.random()-0.5)* amplitudeY
             z = Math.cos(random) *max - ((Math.random()-0.5)*min)
-            const vec3 = new Vector3(x,y,z)
-            const diff = vec3.sub(cameraDirection)
+            vec3.set(x,y,z)
+            // console.log("1", vec3)
+            const diff = vec3.clone().sub(cameraDirection)
+            // console.log("2", vec3)
             // theta is the angle between where the camera is looking vs where the object is in relation to the camera.
             theta = Math.atan2(diff.x, diff.z) - cameraAngle
+            distanceToCamera = Math.abs(vec3.distanceTo(camera.position))
+            // console.log("theta: ", theta)
 
-            // if(abs(theta) < 3){
+            // if(Math.abs(theta) < 3){
             //     if(theta < 0){
             //         //its to the right
             //     }
@@ -46,22 +62,39 @@ const playClicked = (scene, camera) => {
             //         //its to the left
             //     }
             // }
-        // } while((Math.abs(theta) < (- cameraAngle - Math.PI)))
+        // Check if position is outside camera view, TODO: it only accounts for the position of the object, mesh may still be inside
+        // if theta smaller than 0 then the object is to the right of the camera, which is what we want
+            count++
+        } while(!( -spawnAngle - spawnOffset < theta && theta < spawnAngle - spawnOffset ) && count < countMax)
         maxRange =  theta>maxRange?theta:maxRange
         minRange =  theta<minRange?theta:minRange
         // console.log(theta)   
 
         // https://stackoverflow.com/questions/42215829/calculate-target-range-on-circle-three-js
-        asteroid1.position.set(x, y, z)
-        scene.add(asteroid1)
+        // https://stackoverflow.com/questions/29758233/three-js-check-if-object-is-still-in-view-of-the-camera
+        if(count!==countMax){
+            asteroid1.position.set(x, y, z)
+            scene.add(asteroid1)
+        }
     // }
+    console.log(theta)
+    console.log(cameraAngle)
+    console.log(theta+cameraAngle)
+    console.log(-spawnAngle + spawnOffset, spawnAngle + spawnOffset)
     // console.log("maxRange: ", maxRange)   
     // console.log("minRange: ", minRange)   
-    // TODO: add pi to camera to get maxRange/minRange value to compare to theta
-    console.log("theta: ", theta)
-    console.log("camera: ",Math.atan2(cameraDirection.x, cameraDirection.z))   
-    console.log(Math.abs(theta), (cameraAngle - Math.PI), (Math.abs(theta) < ( Math.abs(cameraAngle) + Math.PI)))
-
+    // // TODO: add pi to camera to get maxRange/minRange value to compare to theta
+    // console.log("hFOV*distanceToCamera: ", hFOV*distanceToCamera)
+    // console.log("hFOV/distanceToCamera: ", hFOV/distanceToCamera)
+    // console.log("distanceToCamera/hFOV: ", distanceToCamera/hFOV)
+    // console.log("distanceToCamera: ", distanceToCamera)
+    console.log("hFOV: ", hFOV)
+    // console.log("theta: ", theta)
+    // console.log("camera: ",Math.atan2(cameraDirection.x, cameraDirection.z))   
+    // // console.log("-( cameraAngle + Math.PI): ", -( cameraAngle + Math.PI),"-( cameraAngle - Math.PI): ", -( cameraAngle - Math.PI), (-( cameraAngle - Math.PI) < theta && theta < -( cameraAngle + Math.PI)))
+    // // console.log("-( cameraAngle + hFOV): ", -( cameraAngle + hFOV),"-( cameraAngle - hFOV): ", -( cameraAngle - hFOV), (-( cameraAngle + hFOV) < theta && theta < -( cameraAngle - hFOV)))
+    // console.log(hFOV/2+theta)
+    // // to check if object is in camera view: -hFOV < theta && theta < hFOV
 }
 
 export default playClicked
